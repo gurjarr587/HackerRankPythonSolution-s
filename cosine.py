4ea14cdf6d480786d589a0e01922d83a2a638af1
gurjarr587 <gurjarr587@gmail.com>
2019-02-22 15:59:20 -0500

    Add files via upload

---
+++ cosine.py
@@ -0,0 +1,36 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.corpus import stopwords
+import numpy as np
+import numpy.linalg as LA
+
+train_set = ["The sky is blue.", "The sun is bright."] #Documents
+test_set = ["The sun in the sky is bright."] #Query
+stopWords = stopwords.words('english')
+
+vectorizer = CountVectorizer(stop_words = stopWords)
+#print vectorizer
+transformer = TfidfTransformer()
+#print transformer
+
+trainVectorizerArray = vectorizer.fit_transform(train_set).toarray()
+testVectorizerArray = vectorizer.transform(test_set).toarray()
+print ('Fit Vectorizer to train set', trainVectorizerArray)
+print ('Transform Vectorizer to test set', testVectorizerArray)
+cx = lambda a, b : round(np.inner(a, b)/(LA.norm(a)*LA.norm(b)), 3)
+
+for vector in trainVectorizerArray:
+    print (vector)
+    for testV in testVectorizerArray:
+        print (testV)
+        cosine = cx(vector, testV)
+        print (cosine)
+
+transformer.fit(trainVectorizerArray)
+
+print (transformer.transform(trainVectorizerArray).toarray())
+
+transformer.fit(testVectorizerArray)
+
+tfidf = transformer.transform(testVectorizerArray)
+print (tfidf.todense())
